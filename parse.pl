697ccc9522cd171c1bc0ce107f797e501cfee0e4
Gitomatic <>
2020-02-09 21:52:29 +0100

    fixup! fixup! fixup! fixup! Add code for parsing PDN files
    parse.pl

--- parse.pl
+++ parse.pl
@@ -57,7 +57,7 @@ parse_pdn_flexible([], []) :- !.
 parse_pdn_small(Objects, Codes) :-
   parse_pdn_strict(Objects, Codes)
     -> !
-    : (
+    ; (
       Codes = [Drop|Rest],
       parse_pdn_small(Objects, Rest)
     ).
