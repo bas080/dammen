3e116c649cf4d384f09494b757f1fb50f28e117b
Gitomatic <>
2020-02-10 03:51:07 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add code for parsing PDN files
    dammen.pl

--- dammen.pl
+++ dammen.pl
@@ -187,6 +187,9 @@ pieces_between(From, To, Piece, Board) :-
 % Consider refactoring the top three capture into two fn one for man and one
 % for king
 
+capture(Captures) :-
+  is_list(Captures).
+
 capture(piece(man, Color, From),
         piece(man, Color, To),
         piece(_, Opposite, CaptureField)) :-
