35654aad1261e95cc9b99f34619bd3e5d999ee4e
Gitomatic <user@gitomatic.org>
2020-02-18 17:54:07 +0100

    Draft for a simple server

---
+++ daxy.js
@@ -0,0 +1,22 @@
+
+const http = require('http');
+
+const hash = {}
+
+const daxy = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/pdn' });
+
+  console.log(req.url)
+
+  req.on('data', console.log)
+
+  console.log(hash[req.url])
+
+  // Fire and forget
+  hash[req.url] &&
+    req.pipe(hash[req.url].res)
+
+  hash[req.url] = {req, res}
+});
+
+daxy.listen(8080, '127.0.0.1')
