12e001dda1ad264688db4d4307649e736276a3ca
Gitomatic <user@gitomatic.org>
2020-03-02 21:18:11 +0100

    Add tap todo output for invalid game

--- t/games.t
+++ t/games.t
@@ -6,11 +6,11 @@ plan 1
 
 compile | diagnostics
 
+todo 'Dammen process fails when PDN is invalid'
+
 find ./t/pdn/xx* | parallel \
   -v \
   -L 1 \
   --halt now,fail=1 \
   ./dammen | grep pdn | diagnostics;
 test_success "Valid pdn can be validated"
-
-# TODO: also check if invalid pdn files error.
--- t/test-dependencies.sh
+++ t/test-dependencies.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env mache
 #!/usr/bin/env bash
 
-curl -s https://raw.githubusercontent.com/bas080/bash-tap/master/bash-tap
-cat .local.bashrc
+curl -s https://raw.githubusercontent.com/bas080/bash-tap/e3cff71c808e0413aa40d1b0db987a09ebead6f0/bash-tap
+cat <<'EOF'
+compile() {
+  swipl -O --goal=main --stand_alone=true -o dammen -c ./src/*.pl
+}
+EOF
